b87045815d51166517a7b7defd39ab77fa5aab67
mwovi <petermwovi73@gmail.com>
2023-06-20 12:28:00 +0300

    Edit Notifications.js file to fix modal error display issue.

--- src/Pages/Notifications.js
+++ src/Pages/Notifications.js
@@ -1,20 +1,20 @@
-import React, { useEffect, useState } from 'react';
-import { createUseStyles } from 'react-jss';
-import Card from '../components/Card';
-import { getNotifications } from '../api/notifications';
-import NotificationItem from '../components/NotificationItem';
-import Loader from '../components/Loader';
-import Notification from '../components/Notification';
-import { sortByDate } from '../utils/helpers';
-import { Pagination } from 'antd';
+import React, { useEffect, useState } from "react";
+import { createUseStyles } from "react-jss";
+import Card from "../components/Card";
+import { getNotifications } from "../api/notifications";
+import NotificationItem from "../components/NotificationItem";
+import Loader from "../components/Loader";
+import Notification from "../components/Notification";
+import { sortByDate } from "../utils/helpers";
+import { Pagination } from "antd";
 
 const useStyles = createUseStyles({
   notification: {
     padding: 16,
   },
   Pagination: {
-    display: 'flex',
-    justifyContent: 'flex-end',
+    display: "flex",
+    justifyContent: "flex-end",
   },
 });
 
@@ -22,6 +22,8 @@ export default function Notifications() {
   const [notifications, setNotifications] = useState([]);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
+  const [success, setSuccess] = useState(null);
+
   const [currentPage, setCurrentPage] = useState(1);
   const [notificationsPerPage, setNotificationsPerPage] = useState(15);
   const totalNotifications = notifications?.length;
@@ -40,13 +42,13 @@ export default function Notifications() {
     try {
       setLoading(true);
       const data = await getNotifications({
-        email: 'dnjau@intellisoftkenya.com',
+        email: "dnjau@intellisoftkenya.com",
       });
       const sorted = sortByDate(data?.details);
       setNotifications(sorted);
       setLoading(false);
     } catch (error) {
-      setError('Error fetching notifications');
+      setError("Error fetching notifications");
       setLoading(false);
     }
   };
@@ -56,10 +58,10 @@ export default function Notifications() {
   }, []);
 
   return (
-    <Card title='NOTIFICATIONS'>
+    <Card title="NOTIFICATIONS">
       {error && (
         <Notification
-          type='error'
+          status="error"
           message={error}
           onClose={() => setError(null)}
         />
